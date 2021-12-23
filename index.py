f09526d1be057c7fff211483eafad56e348c09ab
Tyler <>
2021-12-22 19:01:13 -0500

    Color Visuals

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.animation import FuncAnimation
 from numpy.core.fromnumeric import repeat
 
 full_copies = []
+idx = []
+access = []
 
 def main():
+    global full_copies, idx, access
     run = True
     while run:
         print("Welcome to the Sorting Visualizer \n")
@@ -33,9 +36,16 @@ def main():
             for (rectangle, height) in zip(container.patches, full_copies[frames]):
                 rectangle.set_height(height)
                 rectangle.set_color('#1f77b4')
+            
+            idxVal = idx[frames]
+            accessVal = access[frames]
+            if accessVal == 'get':
+                container.patches[idxVal].set_color('black')
+            elif accessVal == 'set':
+                container.patches[idxVal].set_color('red')
             return container
-
-        ani = FuncAnimation(fig, update, frames=range(len(full_copies)), blit=True,
+        
+        ani = FuncAnimation(fig, update, frames=range(len(full_copies)), blit=False,
                                 interval=1000/60, repeat=False)
         plt.show()
         print("Sorted Array: ")
@@ -43,6 +53,9 @@ def main():
         print("Would you like to run again: (Y for yes / N for no)")
         ans = input('Y or N: ')
         if ans == "n" or  ans == "N":
+            full_copies = []
+            idx = []
+            access = []
             print("Ending..")
             run = False
 
@@ -51,11 +64,20 @@ def insertionSort(arr):
     while i < len(arr):
         j = i
         while j > 0 and arr[j-1] > arr[j]:
+            idx.append(j)
+            access.append('get')
             temp = arr[j]
             full_copies.append(np.copy(arr))
-            arr[j] = arr[j-1]
+            
+            idx.append(j)
+            access.append('set')
+            arr[j] = arr[j - 1]
             full_copies.append(np.copy(arr))
-            arr[j-1] = temp
+            
+            
+            idx.append(j-1)
+            access.append('set')
+            arr[j - 1] = temp
             full_copies.append(np.copy(arr))
             j -= 1
             
@@ -69,11 +91,22 @@ def bubbleSort(arr):
             if arr[j + 1] < arr[j]:
                 temp = arr[j]
                 full_copies.append(np.copy(arr))
+                idx.append(j)
+                access.append('get')
+                
                 arr[j] = arr[j + 1]
                 full_copies.append(np.copy(arr))
+
+                idx.append(j)
+                access.append('set')
+                
                 arr[j + 1] = temp
                 full_copies.append(np.copy(arr))
+                idx.append(j+1)
+                access.append('set')
     return arr
+
+
 
 if __name__== "__main__":
     main()
